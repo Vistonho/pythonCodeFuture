0bd55bf5df3ae0e467b1d8f33292e0dfe96089cf
vistonho <vistonho@gmail.com>
2023-11-08 11:23:19 +0300

    test commit

---
+++ 0811_functions/addition.py
@@ -0,0 +1,10 @@
+# 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
+
+def add(x,y):
+    return x + y
+
+print("Введите 2 числа!")
+a = int(input("Первое число: "))
+b = int(input("Второе число: "))
+
+print("Сумма: " + str(add(a,b)))
